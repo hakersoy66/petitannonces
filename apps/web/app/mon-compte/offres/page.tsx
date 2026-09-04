cc810b546e55d32b5f75fb6ee82120fc133a811b
hakersoy66 <hakersoy66@gmail.com>
2026-09-04 18:37:00 +0200

    ui: add account offers route

---
+++ apps/web/app/mon-compte/offres/page.tsx
@@ -0,0 +1,10 @@
+export default function AccountOffersPage() {
+  return (
+    <main style={{padding:'48px 20px',maxWidth:900,margin:'0 auto'}}>
+      <a href="/mon-compte">← Retour</a>
+      <h1 style={{marginTop:20}}>Mes offres</h1>
+      <p style={{marginTop:12}}>Cette section est en cours de finalisation.</p>
+      <a href="/commandes" style={{display:'inline-block',marginTop:20}}>Voir mes commandes</a>
+    </main>
+  );
+}
